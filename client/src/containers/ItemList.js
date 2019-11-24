41f42797b3263d870e16120a0ddcf518c8db3573
Seunghwan <seunghwansohn@gmail.com>
2019-11-24 17:30:47 +0700

    api 불러와서 콜백으로 콘솔출력 구현

--- client/src/containers/ItemList.js
+++ client/src/containers/ItemList.js
@@ -4,6 +4,23 @@ import SearchAppBar from '../components/appBar'
 import { connect } from 'react-redux';
 import {search}  from '../modules/itemList'
 
+
+
+const callApi = async (callback) => {    //node.js api 서버를 호출하는 함수. async는 비동기 처리를 위한 것
+    const response = await fetch('/api/customers');
+    const body = await response.json();  //json 형식으로 받아 body라는 변수에 저장
+    callback(body)
+    return body; //body를 return하여 callApi라는 메소드의 값으로 반환
+  }
+
+const apiLoad = () => {
+    callApi(function(body1) {
+    console.log(body1)
+    })
+};
+
+apiLoad()
+
 const ItemListContainer = (
     {searchKeyword, search}
 ) => {
--- client/src/modules/itemList.js
+++ client/src/modules/itemList.js
@@ -1,4 +1,16 @@
+const axios = require('axios');
+
+// export const fetchArticleDetails = () => {
+//   return function(dispatch) {
+//     return axios.get("/api/customers")
+//       .then(({ data }) => {
+//       dispatch(setArticleDetails(data));
+//     });
+//   };
+// }
+
 const SEARCHKEYWORD = 'itemList/SEARCHKEYWORD';
+const APILOAD = 'itemList/APILOAD';
 
 const initialState = {
     input:'sdfsdf',
@@ -11,6 +23,17 @@ export const search = searchKeyword => ({
   type: SEARCHKEYWORD, 
   searchKeyword
 });
+
+export const apiLoad = ()) => ({
+  type: APILOAD, 
+  itemList
+});
+
+const callApi = async () => {    //node.js api 서버를 호출하는 함수. async는 비동기 처리를 위한 것
+  const response = await fetch('/api/customers');
+  const body = await response.json();  //json 형식으로 받아 body라는 변수에 저장
+  return body; //body를 return하여 callApi라는 메소드의 값으로 반환
+}
 
 function itemList (state = initialState, action) {
     switch (action.type) {
