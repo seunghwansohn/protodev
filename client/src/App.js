76d6272f53a3e958591e3addcb24ac251013ed4a
Seunghwan <seunghwansohn@gmail.com>
2020-01-08 17:39:44 +0700

    로그인 페이지 주소로 라우팅

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ItemListContainer from './containers/main'
 import { Route } from 'react-router-dom';
-
+import LoginPages from './pages/loginPages'
 
 function App() {
   return (
     <div className="App">
-      <Route component = {ItemListContainer} path = '/'/> exact>
+      <Route component = {ItemListContainer} path = '/' exact /> 
+      <Route component = {LoginPages} path = '/login'/> 
     </div>
   );
 }
