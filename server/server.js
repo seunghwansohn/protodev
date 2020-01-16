9ee5b83d594ee3022a7c06b988c259a9107611ca
Seunghwan <seunghwansohn@gmail.com>
2020-01-16 21:07:25 +0700

    sql - 암호화 및 node 연결 성공

--- server/server.js
+++ server/server.js
@@ -1,8 +1,38 @@
 const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require("cors");
+
+
 const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+
+//bodyParser를 통해 모든 서버-클라이언트 통신을 json으로
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true}))
+
+
+const db = require("./models");
+const Role = db.role;
+
+db.sequelize.sync();
+
+app.get('/', (req,res) => {
+  res.send({message: "Welcome to bezkoder application." })
+});
+
+
+require('./routes/auth.routes')(app)
+require('./routes/user.routes')(app)
+
 const port = process.env.PORT || 5000;
+
 
 //formidable 사용위해 필요한 코드
 var formidable = require('formidable'),
@@ -10,9 +40,6 @@ var formidable = require('formidable'),
     util = require('util');
 //--------------------------------
 
-//bodyParser를 통해 모든 서버-클라이언트 통신을 json으로
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true}))
 
 app.get('/api_s/hello', (req,res) => {
     res.send({message: 'hello exp00'})
